b2bc98e67a7d7d4ecc517bf80f66103c710da1c8
VEEETOOORRR <antonni2601.ag@gmail.com>
2025-10-16 20:32:45 -0300

    socorro jesus

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from micropython import const, opt_level
 
 # Imports locais
 from lib.PIDController import PIDController
-from lib.LightSensorArray import LSA
+#from lib.LightSensorArray import LSA
 
 # Imports globais 
 from pybricks.hubs import EV3Brick
@@ -25,30 +25,29 @@ ev3.screen.set_font(Font(size=14))
 
 # Atuadores
 left_motor = Motor(Port.A)
-right_motor = Motor(Port.C)
+right_motor = Motor(Port.D)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=45.0, axle_track=255)
 
 # Sensores
 sensorR = ColorSensor(Port.S4)
 sensorL = ColorSensor(Port.S1)
 
-lsa = LSA(Port.S3)
-ultra = UltrasonicSensor(Port.S2)
+ultra = UltrasonicSensor(Port.S3)
 
 # Parâmetros do controlador PID
-KP = 6
+KP = 4
 KI = const(0)
 KD = const(0)
 controller = PIDController(KP, KI, KD)
 
 # Parâmetros chassi/drivebase
 BASE_VEL = const(150)
-DESVIO_RIGHT = 6
+DESVIO_RIGHT = 0
 DESVIO_LEFT = 0
 
 # Parâmetros sensor de cor
-LIMIAR_PRETO = const(15)
-LIMIAR_BRANCO = const(35)
+LIMIAR_PRETO = const(10)
+LIMIAR_BRANCO = const(60)
 
 # Parâmetros ultrasônico
 DIST_ULTRASONICO = const(100) # em milimetros
@@ -57,34 +56,6 @@ DIST_ULTRASONICO = const(100) # em milimetros
 USAR_SENSOR_LSA = False
 LIMIAR_PRETO_LSA = const(80)
 PESOS_LSA = [1, 2, 3, 4, -4, -3, -2, -1]
-
-# Funções principais
-
-#TODO: fazer os comandos de calibração do sensor LSA funcionarem
-def loop_calibracao():
-    while True:
-        buttons = ev3.buttons.pressed()
-        ev3.screen.print("MODO: CALIBRAÇÃO")
-        wait(500)
-
-        if Button.UP in buttons:
-            lsa.calibrate_white()
-            ev3.screen.print("CALIBRAÇÃO: branco calibrado")
-            ev3.screen.print("novo limite de branco: ", )
-            wait(2000)
-
-        if Button.DOWN in buttons:
-            lsa.calibrate_black()
-            ev3.screen.print("CALIBRAÇÃO: preto calibrado")
-            ev3.screen.print("novo limite de preto: ", )
-            wait(2000)
-
-        if Button.CENTER in buttons:
-            print("CALIBRAÇÃO: Saindo da calibração")
-            wait(1000)
-            break
-        
-        ev3.screen.clear()
 
 def check_ultrasonico():
 
@@ -112,60 +83,22 @@ def check_ultrasonico():
         # se não, não dá pra usar os motores individualmente sem o micropython crashar
         robot.stop()
 
-def check_curvas90(colorLeft, colorRight):
-    
-    # curva 90° para a esquerda
-    if( (colorLeft <= LIMIAR_PRETO) and (colorRight >= LIMIAR_BRANCO) ):
-        ev3.speaker.beep(157, 200)
-        robot.stop()
-        robot.straight(-100)
-        robot.stop()
-        robot.straight(100)
-
-        color_right = sum(sensorR.rgb())
-
-        for i in range(200):
-            color_right = sum(sensorR.rgb())
-
-        while True:
-            color_right = sum(sensorR.rgb())
-
-            if(color_right <= LIMIAR_PRETO):
-                break
-            else:
-                robot.drive(0, 20)
-
-    # toda vez devemos terminar com robot.stop()
-    # se não, não dá pra usar os motores individualmente sem o micropython crashar
-    robot.stop()
-
-    '''
-    # curva 90° para a direita
-    if ( (colorLeft >= LIMIAR_BRANCO) and (colorRight <= LIMIAR_PRETO) ):
-        ev3.speaker.beep(157, 200)
-        robot.stop()
-
-        while True:
-            color_left = sum(sensorL.rgb())
-
-            if(color_left <= LIMIAR_PRETO):
-                break
-            else:
-                robot.drive(0, 20)
-    '''
 
 def check_curvas90_teste(colorLeft, colorRight):
     
     # curva 90° para a esquerda
     if( (colorLeft <= LIMIAR_PRETO) and (colorRight >= LIMIAR_BRANCO) ):
-        left_motor.run_angle(250, 100, wait=False)
-        right_motor.run_angle(250, -100, wait=True)
-
+        right_motor.run_angle(100, 80, wait=False)
+        left_motor.run_angle(100, 80, wait=True)
+        right_motor.run_angle(100, 165, wait=False)
+        left_motor.run_angle(100, -165, wait=True)
+        wait(200)
     elif( (colorRight <= LIMIAR_PRETO) and (colorLeft >= LIMIAR_BRANCO) ):
-        #left_motor.run_angle(250, 215, wait=False)
-        #right_motor.run_angle(250, -215, wait=True)
-        left_motor.run_angle(250, 100, wait=False)
-        right_motor.run_angle(250, -100 , wait=True)
+        right_motor.run_angle(100, 80, wait=False)
+        left_motor.run_angle(100, 80, wait=True)
+        right_motor.run_angle(100, -165, wait=False)
+        left_motor.run_angle(100, 165, wait=True)
+        wait(200)
 
 def calcular_erro_lsa():
     soma_ponderada = 0
@@ -198,33 +131,18 @@ def main_loop():
 
     #check_curvas90(rgb_left, rgb_right)
 
-    if USAR_SENSOR_LSA:
-        erro_lsa = calcular_erro_lsa()
+    color = (colorR + DESVIO_RIGHT) - (colorL + DESVIO_LEFT)
 
-        signal = controller.update(0, erro_lsa)
+    signal = controller.update(0, color)
 
-        #print(erro_lsa)
-        #print(3*signal)
-        print(lsa.read_calibrated())
+    check_curvas90_teste(colorL, colorR)
 
-        left_motor.run(BASE_VEL + 15*signal)
-        right_motor.run(BASE_VEL - 15*signal)
-
-        #print("left: {} // right: {} // erro: {}".format(colorL, colorR, color))
-
-    else:
-        color = (colorR + DESVIO_RIGHT) - (colorL + DESVIO_LEFT)
-
-        signal = controller.update(0, color)
-
-        check_curvas90_teste(colorL, colorR)
-
-        left_motor.run(BASE_VEL + signal)
-        right_motor.run(BASE_VEL - signal)
+    left_motor.run(BASE_VEL + signal)
+    right_motor.run(BASE_VEL - signal)
 
 
 
-        print("left: {} // right: {} // erro: {}".format(colorL, colorR, color))
+    print("left: {} // right: {} // erro: {}".format(colorL, colorR, color))
 
 
 
